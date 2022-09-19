379fadf5aadc00c59921dc6d6238123e74b6627f
Jiatong Han <59948448+JThh@users.noreply.github.com>
2022-09-20 01:48:07 +0800

    Minor update

--- socipy/file-conversion/batch.sh
+++ socipy/file-conversion/batch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-DIR=$(pwd)
 UTILITY="all" # trigger all functions
 
 pip install striprtf tika bs4 # install all dependencies
-
 
 if ["$(UTILITY)" -eq "all"]; then
     for script in ./*.py
